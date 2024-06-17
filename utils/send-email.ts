83827394b6e9339ad55a657183bf51ac94c81cd7
dinofunds <135698451+dinofunds@users.noreply.github.com>
2024-06-17 00:54:59 +0000

    Update send-email.ts

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const endpoint = "https://api.emailjs.com/api/v1.0/email/send";
-const service_id = "service_wdomieg";
-const template_id = "template_kftrzf9";
-const public_key = "c26Mktgwy8ORsDWMA";
+const service_id = "service_scpiclv";
+const template_id = "template_gj9iae7";
+const public_key = "NIhyDX5hopqPok_by";
 
 const sendEmail = (values: any) => {
   const payload = {
